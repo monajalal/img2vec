e7eaad6111e7ca0dbe2eba3bebda7b7e20d35cb7
Mona Jalal <jalal@bu.edu>
2018-10-28 22:35:41 -0400

    added tsne

--- example/get_img_vec.py
+++ example/get_img_vec.py
@@ -12,55 +12,17 @@ input_path = "/scratch2/NAACL2018/shooting_images/result_images/aug2018/related_
 img2vec = Img2Vec()
 vector_fh = open('resnet50_feature_vectors.txt', 'a+')
 
-
-# For each test image, we store the filename and vector as key, value in a dictionary
-pics = {}
-'''for file in os.listdir(input_path):
-    filename = os.fsdecode(file)
-    img = Image.open(os.path.join(input_path, filename))
-    vec = img2vec.get_vec(img)
-    pics[filename] = vec'''
-
-
-
-#pic_name = "test_images/cat.jpg"
-
-#pic = Image.open("test_images/cat.jpg")
-pic = Image.open(input_path + "/969.jpeg")
-#vec = img2vec.get_vec(img)
-
-#print("vec is: ", vec)
-#print("vec shape is: ", len(vec))
-#print("vec shape is: ", vec.shape)
-
 filenames = glob.glob(input_path + "/*.*")
 
 for filename in filenames:
     print(filename)
     img = Image.open(filename)
     nd_arr = img2vec.get_vec(img)
-    print(type(nd_arr))
-    print(nd_arr.T)
-    np.savetxt(vector_fh, nd_arr.T, delimiter='\t')
-
-'''while pic_name != "exit":
-    pic_name = str(input("Which filename would you like similarities for?\n"))
-
-    try:
-        sims = {}
-        for key in list(pics.keys()):
-            if key == pic_name:
-                continue
-
-            sims[key] = cosine_similarity(pics[pic_name].reshape((1, -1)), pics[key].reshape((1, -1)))[0][0]
-
-        d_view = [(v, k) for k, v in sims.items()]
-        d_view.sort(reverse=True)
-        for v, k in d_view:
-            print(v, k)
-
-    except KeyError as e:
-        print('Could not find filename %s' % e)
-
-    except Exception as e:
-        print(e)'''
+    #print(nd_arr.shape)
+    #print(type(nd_arr))
+    #print(nd_arr.T.shape)
+    #print(np.transpose([nd_arr]).shape)
+    #print(nd_arr.tolist())
+    #print(list(nd_arr))
+    curr_line = np.reshape(nd_arr, (1, 2048))
+    np.savetxt(vector_fh, curr_line)
--- example/tsne_python/tsne.py
+++ example/tsne_python/tsne.py
@@ -183,8 +183,10 @@ def tsne(X=np.array([]), no_dims=2, initial_dims=50, perplexity=30.0):
 if __name__ == "__main__":
     print("Run Y = tsne.tsne(X, no_dims, perplexity) to perform t-SNE on your dataset.")
     print("Running example on 2,500 MNIST digits...")
-    X = np.loadtxt("mnist2500_X.txt")
-    labels = np.loadtxt("mnist2500_labels.txt")
+    #X = np.loadtxt("mnist2500_X.txt")
+    X = np.loadtxt("resnet50_feature_vectors.txt")
+    #labels = np.loadtxt("mnist2500_labels.txt")
+    labels = np.loadtxt("august_labels.txt")
     Y = tsne(X, 2, 50, 20.0)
     pylab.scatter(Y[:, 0], Y[:, 1], 20, labels)
     pylab.show()
